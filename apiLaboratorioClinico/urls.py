70ebdd5e917689163ae96e6e81a0f6b4ae14a19e
afabri24 <afabri24@gmail.com>
2023-08-03 03:26:11 -0600

    ya todo funcionando v1.0

--- apiLaboratorioClinico/urls.py
+++ apiLaboratorioClinico/urls.py
@@ -3,5 +3,15 @@ from .views import *
 
 urlpatterns = [
     path('analisis/', analisisView.as_view()),
-    path('analisis/<int:id>', analisisView.as_view())
+    path('analisis/<int:id>', analisisView.as_view()),
+    path('especialidades/', especialidadView.as_view()),
+    path('especialidades/<int:id>', especialidadView.as_view()),
+    path('examenes/', examenView.as_view()),
+    path('examenes/<int:id>', examenView.as_view()),
+    path('pacientes/', pacienteView.as_view()),
+    path('pacientes/<int:id>', pacienteView.as_view()),
+    path('usuarios/', usuarioView.as_view()),
+    path('usuarios/<int:id>', usuarioView.as_view()),
+    path('medicos/', medicoView.as_view()),
+    path('medicos/<int:id>', medicoView.as_view())
 ]
